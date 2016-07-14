3206c4da9f9d0ccca6ef37a9037654c41fbf181b
Razvan <razvan.vasile05@gmail.com>
2016-07-14 17:53:35 +0100

    First three tasks. Still need to find readback value

---
+++ mml_functions_and_scripts/lattice_length.m
@@ -0,0 +1,10 @@
+function lattice_length
+% Function to return the total length of the machine
+middlelayer
+global THERING
+lattice_length=0
+for i=1:length(THERING)
+    lattice_length = lattice_length+THERING{i}.Length
+end
+fprintf('Length of the lattice is: %d.\n', lattice_length)
+end
