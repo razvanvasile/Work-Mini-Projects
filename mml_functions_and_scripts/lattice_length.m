8db099f79bba3e52215d435f526b07ece67bd46f
Razvan <razvan.vasile05@gmail.com>
2016-07-14 17:59:07 +0100

    Print the length of the lattice to the screen

--- mml_functions_and_scripts/lattice_length.m
+++ mml_functions_and_scripts/lattice_length.m
@@ -6,5 +6,7 @@ lattice_length=0
 for i=1:length(THERING)
     lattice_length = lattice_length+THERING{i}.Length
 end
+
+% Print the length of the lattice to the screen
 fprintf('Length of the lattice is: %d.\n', lattice_length)
 end
