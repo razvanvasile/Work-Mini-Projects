74893c0397b40beb29183c0cdac488adaf7feacd
Razvan <razvan.vasile05@gmail.com>
2016-07-14 17:56:59 +0100

    Print the setback value to the screen
    Still need to find a way to print the readback value.

--- mml_functions_and_scripts/mini_project.m
+++ mml_functions_and_scripts/mini_project.m
@@ -1,7 +1,26 @@
-function y = hello_world %#codegen
+function mini_project
+
+% Setup
 middlelayer
-BPMy = getlist('BPMy')
-disp('The number of BPMy elements in the lattice:')
-disp(length(BPMy))
+
+% First task: to print the number of BPMs in the lattice
+%             which equals to the number of BPMy elements.
+BPMy = getlist('BPMy');
+fprintf('The number of BPMy elements in the lattice: %d.\n', length(BPMy))
+
+% Second task: 
+bpm_channels=family2channel('BPMx');
+pvs=getpv('BPMx');
+disp('List of pv names and associated value for all BPMS.\n')
+for i=1:length(pvs)
+    fprintf('BPMx channel name: %s\n', bpm_channels(i,:))
+    fprintf('PV value: %s\n', pvs(i))
 end
 
+% Third task
+quads=getlist('QUAD_')
+sp=getsp('QUAD_')
+for i=1:length(sp)
+    fprintf('Readback value: %d\n', sp(i))
+    fprintf('Setpoint value: %d\n', sp(i))
+end
