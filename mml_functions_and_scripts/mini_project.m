41794333578730ddd5b01d0212d9d06c4bcf58a4
Razvan <razvan.vasile05@gmail.com>
2016-07-15 09:48:53 +0100

    Print readback value for each quad magnet

--- mml_functions_and_scripts/mini_project.m
+++ mml_functions_and_scripts/mini_project.m
@@ -3,8 +3,7 @@ function mini_project
 % Setup
 middlelayer
 
-% First task: to print the number of BPMs in the lattice
-%             which equals to the number of BPMy elements.
+% First task:
 BPMy = getlist('BPMy');
 fprintf('The number of BPMy elements in the lattice: %d.\n', length(BPMy))
 
@@ -13,14 +12,15 @@ bpm_channels=family2channel('BPMx');
 pvs=getpv('BPMx');
 disp('List of pv names and associated value for all BPMS.\n')
 for i=1:length(pvs)
+    % TODO: find specific channel name for each channel
     fprintf('BPMx channel name: %s\n', bpm_channels(i,:))
     fprintf('PV value: %s\n', pvs(i))
 end
 
 % Third task
-quads=getlist('QUAD_')
-sp=getsp('QUAD_')
-for i=1:length(sp)
-    fprintf('Readback value: %d\n', sp(i))
-    fprintf('Setpoint value: %d\n', sp(i))
+sp_values=getpv('QUAD_', 'Setpoint');
+rb_values=getpv('QUAD_');
+for i=1:length(sp_values)
+    fprintf('Readback value: %d\n', rb_values(i))
+    fprintf('Setpoint value: %d\n', sp_values(i))
 end
