08e230810f1e581b321d7d7eda836ca8dee98cb9
Razvan <razvan.vasile05@gmail.com>
2016-07-18 09:09:56 +0100

    Write as a script instead of a function

--- mml_functions_and_scripts/python_epics.m
+++ mml_functions_and_scripts/python_epics.m
@@ -1,13 +1,12 @@
-function python_epics()
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    pv='LI-RF-MOSC-01:FREQ_SET';
-    fprintf('Value of %s is %d.\n', pv, getpv(pv))
+% Simple script to return the pv value off a component
+middlelayer
 
-    try
-        setpv(pv, 4)
-    catch
-        fprintf('Failed to setpv.\n')
-    end
+pv='LI-RF-MOSC-01:FREQ_SET';
+fprintf('Value of %s is %d.\n', pv, getpv(pv))
+
+try
+    % Should fail because it is being called from outside the gateway
+    setpv(pv, 4)
+catch
+    fprintf('Failed to setpv.\n')
 end
-
