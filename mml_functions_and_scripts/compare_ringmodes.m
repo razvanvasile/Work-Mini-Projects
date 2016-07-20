82338396de270a8b14aaca814d1f87cc271888f5
Razvan <razvan.vasile05@gmail.com>
2016-07-20 11:29:04 +0100

    Display the PVs difference between two ring modes

--- mml_functions_and_scripts/compare_ringmodes.m
+++ mml_functions_and_scripts/compare_ringmodes.m
@@ -1,18 +1,15 @@
 function compare_ringmodes
-% Compare PVs in ringmodes 'SRI21' and 'VMX'
-
-% sri21_channels = get_pv_channels();
-
+% Compare PVs for sri21 and vmx ring modes
 sri21_channels = get_pv_channels();
 vmx_channels = get_pv_channels();
 
+sri21_vmx = setdiff(sri21_channels, vmx_channels, 'rows');
+vmx_sri21 = setdiff(vmx_channels, sri21_channels, 'rows');
 
-sri21_vmx = setdiff(sri21_channels, vmx_channels);
-vmx_sri21 = setdiff(sri21_channels, vmx_channels);
-
-% TODO: Find a way to print the contents of a char array row wise
-fprintf('Here are the results, sri21-vmx, %d\n', sri21_vmx);
-
+fprintf('Here are the results, sri21 - vmx\n');
+print_array(sri21_vmx);
+fprintf('Here are the results, vmx - sri21\n');
+print_array(vmx_sri21);
 end
 
 function channel_names = get_pv_channels()
@@ -20,41 +17,31 @@ function channel_names = get_pv_channels()
     
     data = getfamilydata();
     family_names = fieldnames(data);
+    channel_names = char([]);
 
-for i = 1:numel(family_names)% should this have a different name?
+for i = 1:numel(family_names)
     family_data = data.(family_names{i});
     if isfield(family_data, 'Monitor') == 1
         % Assume if Monitor exists then a channel name exists
         fetched_names = family_data.Monitor.ChannelNames();
         fetched_names = unique(fetched_names, 'rows');
-        fprintf('fetched_names = %d\n', size(fetched_names));
-        fprintf('channel_names = %d\n', size(channel_names));
         
         % Check wether the size of the two arrays match
         fetched_size = size(fetched_names);
-        channel_size = size(channel_size);
-
-        % TODO: Find a way to resize these arrays?
-        if fetched_size > channel_size
-            resize_arrays(fetched_size, channel_size);
-        elseif channel_size > fetched_size
-            resize_arrays(channel_size, fetched_size);
+        
+        % Add fetched array to channel names array
+        for i = 1:fetched_size
+            channel_names(end+1, 1:fetched_size(2)) = fetched_names(i,:);
         end
-
-        % Add data together, collumn wise
-        channel_names = [fetched_names; channel_names];
-    elseif isfield(family_data, 'Setpoint')
-        fetched_names = family_data.Setpoint.ChannelNames();
-        fetched_names = unique(fetched_names, 'rows');
-
-        channel_names = [fetched_names; channel_names];
     else
-        fprintf('Could not find a setpoint or monitor field for %s\n', family_data.FamName);
+        fprintf(['Could not find a setpoint or monitor field for '
+            '%s\n'], family_data.FamName);
     end
 end
 end
 
-function result = resize_arrays(target, source)
-    
-
+function print_array(array)
+    for i=1:length(array)
+        fprintf('%s\n', array(i,:));
+    end
 end
