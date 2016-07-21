fe11b69633bbb3333556e1cd5a51ffc4d72f147b
Razvan <razvan.vasile05@gmail.com>
2016-07-21 09:56:52 +0100

    Move useful functions in a separate file for future reference

--- pvs_differences.py
+++ pvs_differences.py
@@ -5,39 +5,10 @@
 import pkg_resources
 pkg_resources.require('aphla')
 import aphla as ap
+from utilities import get_pv_names, get_pvs_from_file
 
-def get_pvs(filepath):
-    ''' Return a list of pvs from a given file '''
-    with open(filepath) as f:
-        contents = f.read().splitlines()
-    return contents
-
-
-# Load the machine
-ap.machines.load('SRI21')
-sri21_pvs = set()
-
-# Find unique pv names for SRI21
-elements = ap.getElements('*')
-
-for element in elements:
-    pvs = element.pv()
-    if (len(pvs) > 0):
-        pv_name = pvs[0].split(':')[0]
-        sri21_pvs.add(pv_name)
-    
-# Load the machine
-ap.machines.load('VMX')
-vmx_pvs = set()
-
-# Find unique pv names for SRI21
-elements = ap.getElements('*')
-
-for element in elements:
-    pvs = element.pv()
-    if (len(pvs) > 0):
-        pv_name = pvs[0].split(':')[0]
-        vmx_pvs.add(pv_name)
+sri21_pvs = get_pv_names('SRI21')
+vmx_pvs = get_pv_names('VMX')
 
 print 'All PVs in \'SRI21\' that are not in \'VMX\''
 print sri21_pvs - vmx_pvs
@@ -48,8 +19,8 @@ print vmx_pvs - sri21_pvs
 print 'Total difference: {0}.'.format(len(vmx_pvs - sri21_pvs))
 
 # Part 2: Compare computed pv sets with recorded data from files
-added_magnets = get_pvs('data/added_magnets.txt')
-removed_magnets = get_pvs('data/removed_magnets.txt')
+added_magnets = get_pvs_from_file('data/added_magnets.txt')
+removed_magnets = get_pvs_from_file('data/removed_magnets.txt')
 
 print 'Intersection of removed_magnets and sri21_pvs: {0}. Total: {1}.' \
     .format(set(added_magnets) & sri21_pvs, len(set(added_magnets) & sri21_pvs))
---
+++ utilities.py
@@ -0,0 +1,27 @@
+# Function to return a list of pvs from a given file
+
+import pkg_resources
+pkg_resources.require('aphla')
+import aphla as ap
+
+def get_pv_names(mode):
+    ''' Given a certain ring mode as a string, return all available pvs  '''
+    ap.machines.load(mode)
+    result = set()
+
+    elements = ap.getElements('*')
+    for element in elements:
+        pvs = element.pv()
+        if(len(pvs) > 0):
+            pv_name = pvs[0].split(':')[0]
+            result.add(pv_name)
+
+    return result
+
+
+def get_pvs_from_file(filepath):
+    ''' Return a list of pvs from a given file '''
+    with open(filepath) as f:
+        contents = f.read().splitlines()
+    return contents
+
