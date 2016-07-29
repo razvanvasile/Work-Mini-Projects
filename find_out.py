63f9eaec91a1f70c6e5979b25c407bddfc62566b
Razvan <razvan.vasile05@gmail.com>
2016-07-29 10:36:45 +0100

    Create script to find all packages and modules inside APHLA
    Rename existent script to differentiate between in requirements
    (methods and classes) and out requirements (modules and packages).

--- find_out.py
+++ find_out.py
@@ -1,8 +1,5 @@
 #!/dls_sw/prod/tools/RHEL6-x86_64/defaults/bin/dls-python
-''' Short script to find all classes inside the APHLA library
-  - print all classes inside the aphla directory
-
-'''
+''' Display all modules in the APHLA file path '''
 import argparse
 from subprocess import Popen, PIPE
 
@@ -11,10 +8,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Location/type of search')
     parser.add_argument("-o", "--object", dest="search_obj",
                         help="argument type: class, function",
-                        metavar="OBJECT", default='class')
-    parser.add_argument("--hide", dest="hide",
-                        help='HIDE private functions/classes',
-                        action='store_true')
+                        metavar="OBJECT", default='module')
 
     required_argv = parser.add_argument_group('required arguments')
     required_argv.add_argument("-p", "--path", dest="path",
@@ -27,37 +21,27 @@ def create_parser():
 def parse_user_settings(argv):
     ''' Used created parser to parse user setings '''
     search_path = argv.path
-    if 'function' in argv.search_obj:
-        search_obj = 'def'
-    else:
-        search_obj = argv.search_obj
-    if argv.hide:
-        search_hide = ' _'
-    else:
-        search_hide = ''
+    search_obj = argv.search_obj
 
-    return search_path, search_obj, search_hide
+    return search_path, search_obj
 
 
-def find_data_on_disk(search_path, search_obj, search_hide):
+def find_data_on_disk(search_path, search_obj):
     ''' Function to find data on disk '''
-    p_1 = Popen(["find", search_path, "-name", "*.py"], stdout=PIPE)
-    p_2 = Popen(["xargs", "grep", '-ne', '^' + search_obj + search_hide],
-                stdin=p_1.stdout, stdout=PIPE)
+    if search_obj == 'module':
+        p_1 = Popen(['find', search_path, '-name', '*.py'], stdout=PIPE)
+    elif search_obj == 'package':
+        p_1 = Popen(['find', search_path, '-name', '__init__.py'], stdout=PIPE)
+    raw_data = p_1.communicate()[0]
     p_1.stdout.close()
-    raw_data = p_2.communicate()[0]
 
     return raw_data
 
 
 def parse_data(raw_data):
     ''' Parse raw data in a more useful format '''
-    # TODO: parse data for when the search object is def
     raw_data = [line.split(':') for line in raw_data.split('\n')]
     del raw_data[-1]
-    for index in range(len(raw_data)):
-        if len(raw_data[index]) == 4:
-            del raw_data[index][3]
 
     return raw_data
 
@@ -65,18 +49,19 @@ def parse_data(raw_data):
 def print_results(data, search_obj):
     ''' Print (parsed) data to the screen '''
     for line in data:
-        print line[0], line[1], '\n', line[2]
+        print line[0]
     print 'Total of {0} \'{1}\' objects found.'.format(len(data), search_obj)
 
 
 def main():
     ''' Docstring '''
     argv = create_parser()
-    search_path, search_obj, search_hide = parse_user_settings(argv)
+    search_path, search_obj = parse_user_settings(argv)
 
-    raw_data = find_data_on_disk(search_path, search_obj, search_hide)
+    raw_data = find_data_on_disk(search_path, search_obj)
     parsed_data = parse_data(raw_data)
     print_results(parsed_data, search_obj)
+
 
 if __name__ == "__main__":
     main()
