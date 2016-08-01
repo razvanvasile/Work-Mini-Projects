5e7645767fdbd60613efd288f811667612f2f9ed
Razvan <razvan.vasile05@gmail.com>
2016-08-01 16:46:24 +0100

    Better format for the output

--- find_out.py
+++ find_out.py
@@ -22,6 +22,9 @@ def parse_user_settings(argv):
     ''' Used created parser to parse user setings '''
     search_path = argv.path
     search_obj = argv.search_obj
+    if search_obj != 'package' and search_obj != 'module':
+        print 'Search object must be \'package\' or \'module\''
+        exit(-1)
 
     return search_path, search_obj
 
@@ -38,18 +41,35 @@ def find_data_on_disk(search_path, search_obj):
     return raw_data
 
 
-def parse_data(raw_data):
+def parse_data(raw_data, search_path):
     ''' Parse raw data in a more useful format '''
     raw_data = [line.split(':') for line in raw_data.split('\n')]
     del raw_data[-1]
+    if search_path.endswith('/'):
+        search_path = search_path[:-1]
+    raw_data = [line[0].split(search_path) for line in raw_data]
+    raw_data = [line[1].split('/') for line in raw_data]
+    for line in raw_data:
+        del line[0]
 
     return raw_data
 
 
 def print_results(data, search_obj):
     ''' Print (parsed) data to the screen '''
+    paths = []
+    classes = []
     for line in data:
-        print line[0]
+        paths.append(line[:-1])
+        classes.append(line[-1])
+
+    path_names = []
+    for line in paths:
+        path_names.append(".".join(line))
+
+    for i in range(len(classes)):
+        print "{:30s}   |   {:10s}".format(path_names[i], classes[i])
+
     print 'Total of {0} \'{1}\' objects found.'.format(len(data), search_obj)
 
 
@@ -59,7 +79,7 @@ def main():
     search_path, search_obj = parse_user_settings(argv)
 
     raw_data = find_data_on_disk(search_path, search_obj)
-    parsed_data = parse_data(raw_data)
+    parsed_data = parse_data(raw_data, search_path)
     print_results(parsed_data, search_obj)
 
 
