b706c1a949b10a7dd4b3206c02de8d4abda088a9
Razvan <razvan.vasile07@gmail.com>
2017-06-02 13:37:52 +0100

    Print each bpmx pv name along with the associated value to stdout

--- pytac/mini_project.py
+++ pytac/mini_project.py
@@ -2,6 +2,7 @@ import pytac.load_csv
 import pytac.epics
 
 def main():
+    # First task: print the number of bpm y elements in the ring.
     lattice = pytac.load_csv.load('VMX', pytac.epics.EpicsControlSystem())
     bpms = lattice.get_elements('BPM')
     bpms_n = 0
@@ -12,6 +13,11 @@ def main():
         print 'There exist {0} BPMy elements in the ring.'.format(bpms_n)
     except:
         print 'Warning! There exists a bpm with no y field.'
+
+    # Second task: Print each bpmx pv along with the associated value.
+    for bpm in bpms:
+        print bpm.get_pv_name('x', 'readback')
+        print bpm.get_pv_value('x', 'readback')
 
 
 if __name__=='__main__':
