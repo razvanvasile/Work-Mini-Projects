c23cdde0376fb76d48b98426d5f353d519250f88
Razvan <razvan.vasile07@gmail.com>
2017-06-02 12:38:03 +0100

    Small modification to print results to a file instead of stdout

--- mml_functions_and_scripts/get_families_count.m
+++ mml_functions_and_scripts/get_families_count.m
@@ -3,14 +3,16 @@ function get_families_count
 
 families = getfamilydata();  % returns a struct
 fields = fieldnames(families); % returns a cell array of strings
+fid = fopen('elements_number_in_families.txt', 'w');
 for i = 1:numel(fields)
     family_name = families.(fields{i}).FamilyName;
     family_count = length(families.(fields{i}).ElementList);
     
     if family_count == 1
-      fprintf('There is %d element in %s\n', family_count, family_name)
+      fprintf(fid ,'There is %d element in %s\n', family_count, family_name)
     else
-      fprintf('There are %d elements in %s\n', family_count, family_name)
+      fprintf(fid, 'There are %d elements in %s\n', family_count, family_name)
     end
 end
+fclose(fid);
 end
